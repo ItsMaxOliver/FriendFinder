204422037626a22ac82634bc4d75d3bb64a4ccdc
MollyLJohnson <mollyl.johnson@att.net>
2018-04-21 17:26:49 -0500

    added placeholder images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
     {
         "name":"Ahmed", 
-        "photo":"https://media.istockphoto.com/photos/happiness-picture-id601008010",
+        "photo":"https://cdn.pixabay.com/photo/2016/03/30/21/53/vintage-1291644_640.jpg",
         "scores":[5,5,4,4,5,5,2,5,4,5]
     },
     {
         "name":"John", 
-        "photo":"https://media.istockphoto.com/photos/happy-laughing-man-picture-id544358212",
+        "photo":"https://cdn.pixabay.com/photo/2016/03/30/21/53/vintage-1291644_640.jpg",
         "scores":[2,2,2,2,2,2,2,2,2,2]
     },
     {
         "name":"Erin", 
-        "photo":"https://media.istockphoto.com/photos/confident-sporty-woman-at-beach-picture-id538335514",
-        "scores":[1,1,3,1,1,1,1,1,1,2]
+        "photo":"https://cdn.pixabay.com/photo/2017/08/27/17/20/person-2686885_640.png",
+        "scores":[1,1,1,1,1,1,1,1,1,1]
     },
     {
         "name":"Stefani", 
-        "photo":"https://media.istockphoto.com/photos/happy-woman-picture-id606210836",
+        "photo":"https://cdn.pixabay.com/photo/2017/08/27/17/20/person-2686885_640.png",
         "scores":[4,4,3,4,1,4,4,5,4,4]
     }
 ]
